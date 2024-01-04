88e4c71c5c8e39a2856a76c06755e317e20d0913
RRodin400 <126524183+RRodin400@users.noreply.github.com>
2024-01-04 13:32:59 -0500

    Create laundryInit.pl

---
+++ laundryInit.pl
@@ -0,0 +1,15 @@
+%%%%% DO NOT SUBMIT THIS FILE
+
+washer(w1).     dryer(d1).
+
+clothes(cl1).   clothes(cl2). hamper(h1).     hamper(h2).
+
+soap(p1).       soap(p2).       cupboard(cbd1). cupboard(cbd2).
+
+softener(sft1). softener(sft2). 
+
+in(cl1,h1,[]).  in(cl2,h2,[]).
+
+in(p1,cbd1,[]). in(p2,cbd2,[]). 
+
+in(sft1,cbd1,[]).       in(sft2,cbd2,[]).
